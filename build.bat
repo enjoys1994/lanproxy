d495a8e4604dcfadc305700d2866d28585d4f0f1
wangguoyan <717338097@qq.com>
2024-06-20 16:47:22 +0800

    optimize

--- build.bat
+++ build.bat
@@ -3,11 +3,28 @@ chcp 65001 > nul
 
 call rmdir /s /q distribution
 
+REM 设置替换字符串的变量
+set "oldKey=7317847dff6a4ee789ad967fd81dfd4d"
+set "newKey=f11e3a4b8fe942f08ba8c00fc4736efc"
+
+REM 设置文件路径
+set "filePath=D:\workspace\code\lanproxy\proxy-client\src\main\resources\config.properties"
+
+powershell -Command "(Get-Content %filePath%) -replace '%oldKey%', '%newKey%' | Set-Content %filePath%"
+
 call mvn clean package
 
+powershell -Command "(Get-Content %filePath%) -replace '%newKey%', '%oldKey%' | Set-Content %filePath%"
 
 cd /d distribution\proxy-client-0.1
 
-call 7z a -tzip -r -spf -aoa ../../proxyClient.zip *
+call 7z a -tzip -r -spf -aoa ../../proxy.zip *
+
+cd ../../
+
+call copy proxy.zip ..\gameHelperServer\uploads\
+
+call del proxy.zip
 
 
+echo http://demo-test.online:8086/down?filename=proxy.zip
---
+++ proxy-client/src/main/resources/openUrl.bat
@@ -0,0 +1,5 @@
+@echo off
+chcp 65001 > nul
+
+
+cmd /c start  https://support.microsoft.com/zh-cn/windows/%E5%A6%82%E4%BD%95%E4%BD%BF%E7%94%A8%E8%BF%9C%E7%A8%8B%E6%A1%8C%E9%9D%A2-5fe128d5-8fb1-7a23-3b8a-41e636865e8c#ID0EDD=Windows_10
